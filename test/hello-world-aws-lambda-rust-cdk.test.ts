4eb0a109d4a504874dc0d6944319cf41a1173233
Matt McGivney <matt@mattmcgivney.com>
2024-01-14 07:53:47 -0700

    Initial commit

---
+++ test/hello-world-aws-lambda-rust-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HelloWorldAwsLambdaRustCdk from '../lib/hello-world-aws-lambda-rust-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-world-aws-lambda-rust-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HelloWorldAwsLambdaRustCdk.HelloWorldAwsLambdaRustCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
